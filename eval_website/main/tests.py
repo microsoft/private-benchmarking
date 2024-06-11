c44790c8c4c8ea498361a530c7931e9d779a79fd
Tanmay Rajore <t-trajore@microsoft.com>
2024-06-11 20:15:51 +0530

    fixed django and added tests

--- eval_website/main/tests.py
+++ eval_website/main/tests.py
@@ -1,5 +1,39 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-from django.test import TestCase
 
-# Create your tests here.
+# # Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from main.models import ModelArchitecture, Dataset, Evaluation
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.model = ModelArchitecture.objects.create(name='test_model', user=self.user)
+        self.dataset = Dataset.objects.create(name='test_dataset', user=self.user)
+        self.evaluation = Evaluation.objects.create(user=self.user, model=self.model, dataset=self.dataset,ip_address='127.0.0.1',port=8000)
+
+    def test_homepage(self):
+        response = self.client.get(reverse('main:homepage'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_request(self):
+        response = self.client.get(reverse('main:register'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_request(self):
+        response = self.client.get(reverse('main:login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout_request(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('main:logout'))
+        self.assertEqual(response.status_code, 302)  # Should redirect after logout
+    
+    def test_profile_request(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('main:user_profile', args=[self.user.id]))
+        self.assertEqual(response.status_code, 200)
